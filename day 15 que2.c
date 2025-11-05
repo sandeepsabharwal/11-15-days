2c794f881981da756ef04c5317d39ff39035b82b
Sandeep Sabharwal <sabharwalsandeep989@gmail.com>
2025-11-05 17:24:17 +0530

    Create day 15 que2.c

---
+++ day 15 que2.c
@@ -0,0 +1,19 @@
+# Write a program to reverse a given number.
+
+#include <stdio.h>
+
+int main() {
+    int num, reversed = 0;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    while (num != 0) {
+        int digit = num % 10;
+        reversed = reversed * 10 + digit;
+        num /= 10;
+    }
+
+    printf("Reversed number: %d\n", reversed);
+    return 0;
+}
