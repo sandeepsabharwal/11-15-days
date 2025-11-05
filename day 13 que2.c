b2a32c834488cfa89b6de24adb9b10f84193bd5a
Sandeep Sabharwal <sabharwalsandeep989@gmail.com>
2025-11-05 17:20:02 +0530

    Create day 13 que2.c

---
+++ day 13 que2.c
@@ -0,0 +1,16 @@
+# Write a program to print numbers from 1 to n.
+
+#include <stdio.h>
+
+int main() {
+    int n;
+
+    printf("Enter a positive integer n: ");
+    scanf("%d", &n);
+
+    for (int i = 1; i <= n; i++) {
+        printf("%d ", i);
+    }
+
+    return 0;
+}
