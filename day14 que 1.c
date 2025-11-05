186b25adbd3ed17db4b74556ab02b1dbbf4a4362
Sandeep Sabharwal <sabharwalsandeep989@gmail.com>
2025-11-05 17:20:51 +0530

    Create day14 que 1.c

---
+++ day14 que 1.c
@@ -0,0 +1,17 @@
+# Write a program to print the sum of the first n odd numbers.
+
+#include <stdio.h>
+
+int main() {
+    int n, sum = 0;
+
+    printf("Enter a positive integer n: ");
+    scanf("%d", &n);
+
+    for (int i = 1; i <= 2*n - 1; i += 2) {
+        sum += i;
+    }
+
+    printf("Sum of first %d odd numbers is: %d\n", n, sum);
+    return 0;
+}
