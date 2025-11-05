e78b871e4e0ec62f1a5b08fc523b0bfcb9c07147
Sandeep Sabharwal <sabharwalsandeep989@gmail.com>
2025-11-05 17:21:54 +0530

    Create day 14 que1.c

---
+++ day 14 que1.c
@@ -0,0 +1,25 @@
+# Write a program to print the product of even numbers from 1 to n.
+
+
+#include <stdio.h>
+
+int main() {
+    int n;
+    long long product = 1;
+    int found = 0;
+
+    printf("Enter a positive integer n: ");
+    scanf("%d", &n);
+
+    for (int i = 2; i <= n; i += 2) {
+        product *= i;
+        found = 1;
+    }
+
+    if (found)
+        printf("Product of even numbers from 1 to %d is: %lld\n", n, product);
+    else
+        printf("No even numbers in the range.\n");
+
+    return 0;
+}
